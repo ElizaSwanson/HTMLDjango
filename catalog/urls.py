e6e592f321afde492f701c649149ce2d37f623d4
Elizaveta Nasytko <Liz99246@gmail.com>
2025-01-18 21:49:20 +0300

    24 homework

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,9 +6,11 @@ from catalog.views import product_list, product_detail
 from catalog.apps import CatalogConfig
 
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('product_list/', product_list, name='product_list'),
-    path('catalog/<int:pk>/', product_detail, name='product_detail'),
+    path('', product_list, name='product_list'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
 ]
 
 if settings.DEBUG:
